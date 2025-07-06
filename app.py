4294b3f69f0ca62ac0c7eeb30f08fdeb88e25233
Aromchik <arturkamorgan@gmail.com>
2025-07-06 18:28:18 +0300

    Connect to DataBase

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request, url_for
 import easyocr
 import google.generativeai as genai
 import os
-
-
-app = Flask(__name__)
+import mysql.connector
+from config import DB_CONFIG
 
 API = input("Введите Google API ключ: ")
+
+app = Flask(__name__)
 
 reader = easyocr.Reader(['en', 'ru'])
 genai.configure(api_key=API)
@@ -26,6 +27,8 @@ def main():
         filepath= os.path.join(UPLOAD_FOLDER, filename) # os.path.join склеивает в единый путь, с учётом операционной системы
         file.save(filepath)
 
+        photo_path = filepath.replace("\\", "/")  # для Windows
+
 
         result = reader.readtext(filepath)
         extracted_text = " ".join([text[1] for text in result])
@@ -33,6 +36,15 @@ def main():
         target_language = request.form['language']
         response = model.generate_content(f"Переведи на {target_language} следующий текст: {extracted_text}")
         translation = response.text
+
+
+
+        conn=mysql.connector.connect(**DB_CONFIG)
+        cursor=conn.cursor()
+        cursor.execute('INSERT INTO user (photo_path,lang,text_before,text_after) VALUES (%s,%s,%s,%s)',(photo_path, target_language,extracted_text,translation))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
     return render_template('main.html', translation=translation)
 
